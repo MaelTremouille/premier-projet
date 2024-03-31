17dcec7e02078dc45c34e5134d0b8de69b1c4da2
Maël Trémouille <tremart82@gmail.com>
2024-04-01 01:32:00 +0200

    test github pilot

--- HelloJava.java
+++ HelloJava.java
@@ -6,3 +6,13 @@ class HelloWorld {
         System.out.println(message); 
     }
 }
+/**
+ * HelloJava
+ */
+public class HelloJava {
+    
+        public static void main(String[] args) {
+            System.out.println("Hello Javca");
+        }
+    
+}
